318df0b27c0a62acaf1da6f33e8882f4b3086349
mercklyzer <mercklyzer@gmail.com>
2026-09-12 11:27:34 +0800

    chore: initial files

---
+++ src/main/java/com/lyzer/lyzerrecime/LyzerRecimeApplication.java
@@ -0,0 +1,13 @@
+package com.lyzer.lyzerrecime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyzerRecimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyzerRecimeApplication.class, args);
+    }
+
+}
